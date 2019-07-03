d6a21181397ab21cd3bfa69b172c875a6912299e
ayancovich <ayancovich@endava.com>
2019-07-03 09:39:49 -0300

    Removed spring security

--- src/main/java/com/endava/RegistryApplication.java
+++ src/main/java/com/endava/RegistryApplication.java
@@ -3,9 +3,6 @@ package com.endava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -15,12 +12,5 @@ public class RegistryApplication {
 		SpringApplication.run(RegistryApplication.class, args);
 
 	}
-	
-	@EnableWebSecurity
-	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable();
-		}
-	}
+
 }
